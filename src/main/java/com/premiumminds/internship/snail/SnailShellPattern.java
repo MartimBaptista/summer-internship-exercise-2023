1c110dd71ac8a7933ddf12918e07a9a27bf72d37
MartimBaptista <martimdsbaptista@gmail.com>
2023-06-03 14:56:20 +0100

    added more comments and a simple input check

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -13,40 +13,50 @@ class SnailShellPattern implements ISnailShellPattern {
    * Method to get snailshell pattern
    * 
    * @param matrix matrix of numbers to go through
-   * @return order array of values thar represent a snail shell pattern
+   * @return order array of values thar represent a snail shell pattern, empty array if invalid input matrix
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
     return executor.submit(() -> {
+      //Simple empty matrix and squre matrix check
+      //assuming that matrix[0].legth == matrix[any].legth (might not)
+      if(matrix.length == 0 || matrix[0].length == 0 || matrix.length != matrix[0].length){ 
+        return new int[0];
+      }
+
+      //Array that will be returned
       int[] res = new int[matrix.length * matrix.length];
       int resCounter = 0;
-
+      
+      //Shrinkage represents how many outside layers have been read
       int shrinkage = 0;
       while(shrinkage * 2 < matrix.length){
 
-        //shrikage reverted
+        //shrikage reverted (used like shrinkage but for the right and bottom side of the snail)
         int revShrinkage = matrix.length - shrinkage - 1;
 
-        //Moving right
+        //Moving right (Top side)
         for (int i = shrinkage; i <= revShrinkage; i++) {
           res[resCounter++] = matrix[shrinkage][i];
         }
         
-        //Moving down
+        //Moving down (Right side) *
         for (int i = shrinkage + 1; i <= revShrinkage; i++) {
           res[resCounter++] = matrix[i][revShrinkage];
         }
 
-        //Moving left
+        //Moving left (Bottom side) *
         for (int i = revShrinkage - 1; i >= shrinkage; i--) {
           res[resCounter++] = matrix[revShrinkage][i];
         }
 
-        //Moving up
+        //Moving up (Left side) *
         for (int i = revShrinkage - 1; i >= shrinkage + 1; i--) {
           res[resCounter++] = matrix[i][shrinkage];
         }
 
         shrinkage++;
+        //* Right, Bottom and Left side i counters start with an ofset of 1 
+        // to jump over the corner values that were read by the previous movement
       }
 
       return res;
